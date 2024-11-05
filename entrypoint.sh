1b2fd914753d33adda4b81bfd4f635d2c2c0fa25
fab5052 <f.beaujois@gmail.com>
2024-11-05 21:29:54 +0100

    entrypt

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env sh
 
+DIGITS=10
+
+if [ -n "$1" ]; then
+    DIGITS=$1
+fi
+
+echo "scale=$DIGITS;4*a(1)" > /pi_expression
+time bc -l /pi_expression
+
 echo "scale=10;4*a(1)" > /pi_expression
 time bc -l /pi_expression
